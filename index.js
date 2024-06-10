c3b99c6cba74d53f4e25fe6116aa7c828f369776
Williams Hernández <hernandw@gmail.com>
2024-06-10 19:41:54 -0400

    :sparkles: configuracion del servidor

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import routes from './routes/router.js'
+import { engine } from 'express-handlebars';
+import expressFileUpload from 'express-fileupload';
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//Motor de Plantilla
+app.engine('handlebars', engine({
+    extname: '.hbs',
+}));
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+
+//Carpeta publica
+app.use(express.static('public'));
+
+//Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+;
+app.use(expressFileUpload());
+
+//Routes
+app.use('/', routes)
+
+
+app.listen(PORT, () => console.log(`Example app listening on port http://localhost:${PORT}`));
