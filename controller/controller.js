b921c8922f29b2b3c18dd6e766437bb71913b106
Williams Hernández <hernandw@gmail.com>
2024-06-10 20:40:43 -0400

    :art: formulario de registro

--- controller/controller.js
+++ controller/controller.js
@@ -3,3 +3,21 @@ export const home = (req, res) => {
         title: 'Home Page'
     })
 } 
+
+export const about = (req, res) => {
+    res.render('about', {
+        title: 'About Page'
+    })
+}
+
+export const contact = (req, res) => {
+    res.render('contact', {
+        title: 'Contact Page'
+    })
+}
+
+export const addGuitarForm = (req, res) => {
+    res.render('addGuitar', {
+        title: 'Guitar Form'
+    })
+}
--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,14 @@
 import express from 'express';
-import { home } from '../controller/controller.js';
+import { home, about, contact, addGuitarForm } from '../controller/controller.js';
 const router = express.Router()
 
 router.get('/', home)
+
+router.get('/about', about)
+
+router.get('/contact', contact)
+
+router.get('/addGuitar', addGuitarForm)
 
 router.get('*', (req, res)=>{
 res.send('404 - page not found')
